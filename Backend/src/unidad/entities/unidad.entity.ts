c04fdafa75358dac377b828bc7986075570c6337
Esteban <aballayesteban@gmail.com>
2025-08-28 10:06:22 -0300

    Merge unidad-viaje y reacomodo de archivos

--- Backend/src/unidad/entities/unidad.entity.ts
+++ Backend/src/unidad/entities/unidad.entity.ts
@@ -2,31 +2,26 @@ import { Entity, PrimaryGeneratedColumn, Column, ManyToOne } from 'typeorm';
 import { Camion } from './camion.entity';
 import { Semirremolque } from './semirremolque.entity';
 import { Acoplado } from './acoplado.entity';
-import { Transportista } from ; // transportista
-import { Viaje } from ;
+import { Transportista } from ; 
+import { Viaje } from '../../viaje/entities/viaje.entity';
 
 @Entity('unidad')
 export class Unidad {
   @PrimaryGeneratedColumn()
   id: number;
 
-  // Relación con Camión
   @ManyToOne(() => Camion, { eager: true })
   Camion: Camion;
 
-  // Relación con Semirremolque
   @ManyToOne(() => Semirremolque, { eager: true })
   semiremolque: Semirremolque;
 
-  // Relación con Acoplado
   @ManyToOne(() => Acoplado, { eager: true })
   acoplado: Acoplado;
 
-  // Relación con Transportista (Usuario)
   @ManyToOne(() => Transportista, { eager: true })
   transportista: Transportista;
 
-  // Campo calculado (no se guarda, se expone por DTO o getter)
   @Column({ type: 'decimal', precision: 10, scale: 2, nullable: true })
   subtotal: number;
 
