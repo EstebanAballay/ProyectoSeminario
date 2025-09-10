e2226a7e5aac0ec396abc7d149677ce52e69207e
Lourdes Bonino <boninolourdes8@gmail.com>
2025-09-10 14:16:29 -0300

    Login terminado

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './login.component/login.component';
+import { PaginainicioComponent } from './paginainicio/paginainicio.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'paginainicio', pathMatch: 'full' }, // ← cambio aquí
+  { path: 'login', component: LoginComponent },
+  { path: 'paginainicio', component: PaginainicioComponent },
+  { path: '**', redirectTo: 'paginainicio' } // opcional: redirigir rutas inválidas al inicio
+];
