00702db74b2f368de5a14b9f050b06c933cd144c
Lourdes Bonino <boninolourdes8@gmail.com>
2025-09-16 15:18:54 -0300

    Se logró el marge

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -1,10 +1,15 @@
 import { Routes } from '@angular/router';
-import { PaginainicioComponent } from './paginainicio/paginainicio.component';
+import { LoginComponent } from './login.component/login.component';
+import { RegisterComponent } from './register.component/register.component'; 
+import { PaginainicioComponent } from './paginainicio/paginainicio.component';  
+import { MenuComponent } from './menu.component/menu.component';  
+import { NuevoViajeComponent } from './nuevo-viaje.component/nuevo-viaje.component';
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'paginainicio', pathMatch: 'full' }, // ← cambio aquí
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'paginainicio', component: PaginainicioComponent },
-  { path: '**', redirectTo: 'paginainicio' } // opcional: redirigir rutas inválidas al inicio
+  { path: 'menu', component: MenuComponent },
+  { path: 'nuevoviaje', component: NuevoViajeComponent }
 ];
